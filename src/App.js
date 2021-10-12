8e6239e4973aacc4a8faaee03655a8ade689898c
HungHCC <buisihung321@gmail.com>
2021-10-12 07:58:21 +0700

    Init project with react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/screen-1">Screen 1</Link>
+        </li>
+        <li>
+          <Link to="/screen-2">Screen 2</Link>
+        </li>
+        <li>
+          <Link to="/screen-3">Screen 3</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/screen-1" />
+        </Route>
+        <Route path="/screen-1">
+          <div>Screen 1</div>
+        </Route>
+        <Route path="/screen-2">
+          <div>Screen 2</div>
+        </Route>
+        <Route path="/screen-3">
+          <div>Screen 3</div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
