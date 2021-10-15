4b1e54cb424af3db2645b1617d50113229274980
HungHCC <buisihung321@gmail.com>
2021-10-15 08:47:08 +0700

    Config styling

---
+++ src/theme/index.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const theme = {};
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Mali',Arial, Helvetica, sans-serif;
+  }
+`;
+
+export default theme;
