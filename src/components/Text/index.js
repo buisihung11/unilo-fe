1f6cace2a4dc4ea6a1988d60651b84fd64f41f90
HungHCC <buisihung321@gmail.com>
2021-10-15 14:27:47 +0700

    Add header and footer for dialog

---
+++ src/components/Text/index.js
@@ -0,0 +1,3 @@
+import Text from "./Text.style";
+
+export default Text;
--- src/pages/Demo/Demo.js
+++ src/pages/Demo/Demo.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { Box, Button, Dialog } from "../../components";
-import { StyledUnilo, StyledUniloWrapper } from "../../components/styles/StyledUnilo";
+import {
+  StyledUnilo,
+  StyledUniloWrapper,
+} from "../../components/styles/StyledUnilo";
+import Text from "../../components/Text";
 
 const Demo = () => {
   const [showDialog, setshowDialog] = useState(false);
@@ -8,7 +12,22 @@ const Demo = () => {
     <StyledUniloWrapper>
       <StyledUnilo>
         <Button onClick={() => setshowDialog(true)}>Show dialog</Button>
-        <Dialog visible={showDialog} onClose={() => setshowDialog(false)} />
+        <Dialog
+          visible={showDialog}
+          onClose={() => setshowDialog(false)}
+          headerTitle={
+            <Text as="h1" fontWeight="bold" fontSize={2} color="#fff">
+              Thông báo
+            </Text>
+          }
+          footer={
+            <Box position="absolute" mx="auto">
+              <Button onClick={() => setshowDialog(false)}>Nhận quà</Button>
+            </Box>
+          }
+        >
+          lorem
+        </Dialog>
       </StyledUnilo>
     </StyledUniloWrapper>
   );
